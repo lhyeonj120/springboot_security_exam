978fb7a807d7a11f0266fa6e49dc582eab3971c6
lhyeonj120 <isk04387@gmail.com>
2023-03-23 12:21:29 +0900

    my code

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -17,13 +17,13 @@ import javax.servlet.Filter;
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private UserService userService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private BCryptPasswordEncoder passwordEncoder;
     private Environment env;
 
-    private WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder){
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder){
         this.env = env;
         this.userService = userService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.passwordEncoder = bCryptPasswordEncoder;
     }
 
     //권한 관련 메소드
@@ -31,16 +31,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeHttpRequests().antMatchers("/users").permitAll(); //'/users'로 들어오는 것은 허락해줘
-
-        // h2 db 접속 원활하도록 설정
         http.authorizeHttpRequests().antMatchers("/h2-console/**").permitAll();
-        http.headers().frameOptions().disable();
-
 
         http.authorizeRequests().antMatchers("/**")
                 .hasIpAddress("localhost")
                 .and()
                 .addFilter(getAuthenticationFilter());
+
+
+        http.headers().frameOptions().disable();
     }
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception{
@@ -53,6 +52,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //userDetailsService가 id와 password 처리 해주는데 userDetailsService 누구니
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
     }
 }
