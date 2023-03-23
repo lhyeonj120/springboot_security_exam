903e5d0d467950592665b1714a9326bc1d80dd08
lhyeonj120 <isk04387@gmail.com>
2023-03-23 11:53:36 +0900

    add WebSecurity

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,16 +1,30 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    private WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     //권한 관련 메소드
     @Override
@@ -39,6 +53,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //userDetailsService가 id와 password 처리 해주는데 userDetailsService 누구니
-        auth.userDetailsService()
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
